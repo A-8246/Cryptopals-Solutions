c4cda0de41e3d0b812b53c429a187506ebda7467
A-8246 <62061147+A-8246@users.noreply.github.com>
2021-01-13 22:47:39 +0530

    Update Challange17.py

--- Set3/Challange17.py
+++ Set3/Challange17.py
@@ -1,3 +1,7 @@
+#https://perso.heavyberry.com/articles/2015-12/cryptopals3
+#https://blog.skullsecurity.org/2013/padding-oracle-attacks-in-depth
+#https://robertheaton.com/2013/07/29/padding-oracle-attack/
+
 from Crypto.Cipher import AES
 import base64
 import random
